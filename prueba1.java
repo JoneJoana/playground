f17cbdd2d29bfead5c87226e2888986bb16acd8a
Joana <joanajanefdez@gmail.com>
2022-02-16 11:48:46 +0100

    Revert "probar to run proyecto"
    This reverts commit a1290c0089da05dd640b03faffb76a88593c4274.

--- prueba1.java
+++ prueba1.java
@@ -1,14 +1,11 @@
-public abstract class prueba1 {
+public class prueba1 {
     int prueba = 4;
     String oscar = "pichun";
     boolean covidOscar = true;
     String joana = "pichuna";
     boolean covidJoana = false;
 
-    String hola = sayHi();
-
-    public String sayHi() {
+    public void sayHi() {
         System.out.print("Hi");
-        return null;
     }
 }
